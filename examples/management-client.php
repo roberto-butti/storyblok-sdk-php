79fd83d6f58d78ad455b87a0c54e19d0e8e5f40b
Roberto Butti <roberto.butti@gmail.com>
2022-09-14 07:25:03 +0200

    Update management-client.php

--- examples/management-client.php
+++ examples/management-client.php
@@ -9,10 +9,13 @@ $dotenv->load();
 
 $token = (string)$_ENV['STORYBLOK_ACCESS_TOKEN'];
 
-$sdk = new ManagementSdk();
+$sdk = ManagementSdk::make($token);
 // https://www.storyblok.com/docs/api/management#core-resources/spaces/spaces
-$spaces = $sdk->token($token)->spaces()->all();
+$spaces = $sdk->spaces()->all();
+
 $space = [];
 foreach ($spaces["spaces"] as $space) {
     echo str_pad($space["id"], 10, " ", STR_PAD_LEFT) . " " . $space["name"] . PHP_EOL;
 }
+
+$spaces = $sdk->spaces()->space('74054')->get();
