6ee37a52bce1c6303d2819cbe9060168cab2abb9
Roberto Butti <roberto.butti@gmail.com>
2022-09-26 18:24:12 +0200

    Adding tests for BaseSDK class

--- src/HttpClient/BaseSdk.php
+++ src/HttpClient/BaseSdk.php
@@ -10,20 +10,36 @@ abstract class BaseSdk
     protected ClientBuilder $clientBuilder;
     protected Options $options;
 
+    /**
+     * Get the UriInterface of the current endpoint
+     * @return UriInterface
+     */
     public function getUri(): UriInterface
     {
         return $this->options->getUri();
     }
 
+    /**
+     * Return the URI (string) of the current endpoint
+     * For example:
+     * - https://api-us.storyblok.com/v2/cdn
+     * - https://api.storyblok.com/v2/cdn
+     * @return string
+     */
     public function getUriString(): string
     {
         return $this->options->getUriString();
     }
+
     public function getHttpClient(): HttpMethodsClientInterface
     {
         return $this->clientBuilder->getHttpClient();
     }
 
+    /**
+     * Returns the user agent
+     * @return string
+     */
     public function getUserAgent(): string
     {
         return 'PHP Custom SDK';
--- tests/SdkSetupTest.php
+++ tests/SdkSetupTest.php
@@ -7,9 +7,11 @@ test('ContentDeliverySdk initialization', function () {
     $s = new ContentDeliverySdk();
     expect($s->getUri()->getHost())->toEqual("api.storyblok.com");
     expect($s->getUriString())->toEqual("https://api.storyblok.com/v2/cdn");
+    expect($s->getUserAgent())->toEqual("PHP Custom SDK");
+    expect($s->getHttpClient())->toBeInstanceOf(\Http\Client\Common\HttpMethodsClient::class);
+
 
     $s = ContentDeliverySdk::make()->regionUs();
-
     expect($s->getUri()->getHost())->toEqual("api-us.storyblok.com");
     expect($s->getUriString())->toEqual("https://api-us.storyblok.com/v2/cdn");
 });
